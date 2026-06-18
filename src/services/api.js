c63f43c08df6edba7c7988679b3758d099283a5a
CastleDev04 <bycasatledev@gmail.com>
2026-06-18 16:31:12 -0300

    firts commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { authService } from './auth.service';
 
-const API_URL = 'https://backend-barberia-4rzl.onrender.com' || 'http://localhost:3000/api';
+const API_URL = 'https://backend-barberia-4rzl.onrender.com/api' || 'http://localhost:3000/api';
 
 const api = axios.create({
   baseURL: API_URL,
